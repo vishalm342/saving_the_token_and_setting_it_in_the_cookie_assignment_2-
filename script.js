954585654a2aa6c38e082e6e322a4be533eef205
vishalm342 <vishalm8656@gmail.com>
2025-04-17 09:15:06 +0530

    token encrypted

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+
+const ENCRYPTION_KEY = crypto.randomBytes(32);
+const IV = crypto.randomBytes(16); 
+
 const encrypt = (payload, secret) => {
-  // your code here and return token
+  const token = jwt.sign(payload, secret, { expiresIn: '1h' });
+
+  const cipher = crypto.createCipheriv('aes-256-cbc', ENCRYPTION_KEY, IV);
+  let encrypted = cipher.update(token, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+
+  return { encryptedToken: encrypted, iv: IV.toString('hex') };
 };
 
 module.exports = encrypt;
